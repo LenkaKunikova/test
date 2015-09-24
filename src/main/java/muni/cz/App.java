e32d8b187f5fd1d57327651f9f7e2d4c81c12046
Lenka Kunikova <xkunikov@nymfe37.fi.muni.cz>
2015-09-24 11:15:33 +0200

    first adding

---
+++ src/main/java/muni/cz/App.java
@@ -0,0 +1,13 @@
+package muni.cz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Lenka!" );
+    }
+}
